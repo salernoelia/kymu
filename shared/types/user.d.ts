2f44c0c8613d9638fe9f119f991d9a21fa647f93
salernoelia <putopet@gmail.com>
2025-02-22 00:21:47 +0100

    rm: enum UserTypes
    it was bloat

--- shared/types/user.d.ts
+++ shared/types/user.d.ts
@@ -3,17 +3,3 @@ export interface User {
   email: string;
   bio: string;
 }
-
-/*
-const UserTypeOptions = [
-  { label: 'Patient', value: 'patient' },
-  { label: 'Caregiver', value: 'caregiver' },
-  { label: 'Therapist', value: 'therapist' },
-];
-*/
-
-export enum UserTypes {
-  Patient = "patient",
-  Caregiver = "caregiver",
-  Therapist = "therapist",
-}
